73778d4f468dea35e12324848bbb7935c9736568
Prathamesh Kulkarni <pratham.kulkarni1999@gmail.com>
2023-04-29 11:12:52 +0200

    Created a URL shortener using Bitly API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [longUrl, setLongUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch('https://api-ssl.bitly.com/v4/shorten', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.REACT_APP_BITLY_ACCESS_TOKEN}`
+      },
+    
+      body: JSON.stringify({ long_url: longUrl })
+    });
+    const data = await response.json();
+    setShortUrl(data.link);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>URL Shortener</h1>
+      <form onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label htmlFor='long-url'>Long URL</label>
+          <input type='text' id='long-url'
+          name='long-url'
+          value={longUrl}
+          onChange={(event) => setLongUrl(event.target.value)}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='short-url'>Short URL</label>
+          <input type='text' id='short-url'
+          name='short-url'
+          value={shortUrl}
+          readOnly
+          />
+        </div>
+        <button type='submit'>Shorten URL</button>
+      </form>
     </div>
   );
 }
